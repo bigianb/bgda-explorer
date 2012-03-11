2a3776344e47918c19b6e223638d7108aa1ce6cd
bigianb@gmail.com <bigianb@gmail.com@e1ae63d9-c44a-27ee-ff2d-4c7a2df71a3b>
2012-03-11 22:57:29 +0000

    anim parsing

--- WorldExplorer/WorldExplorer/DataLoaders/AnmDecoder.cs
+++ WorldExplorer/WorldExplorer/DataLoaders/AnmDecoder.cs
@@ -30,18 +30,60 @@ namespace WorldExplorer.DataLoaders
             int endIndex = startOffset + length;
             AnimData animData = new AnimData();
             animData.NumBones = DataUtil.getLEInt(data, startOffset);
+            animData.Offset4Val = DataUtil.getLEInt(data, startOffset + 4);
+            int offset8Val = startOffset + DataUtil.getLEInt(data, startOffset + 8);
+
+            AnimFrame frame = new AnimFrame();
+
+            frame.x = DataUtil.getLEShort(data, offset8Val) / 64.0;
+            frame.y = DataUtil.getLEShort(data, offset8Val+2) / 64.0;
+            frame.z = DataUtil.getLEShort(data, offset8Val+4) / 64.0;
+
+            frame.a = DataUtil.getLEShort(data, offset8Val + 6) / 4096.0;
+            frame.b = DataUtil.getLEShort(data, offset8Val + 8) / 4096.0;
+            frame.c = DataUtil.getLEShort(data, offset8Val + 10) / 4096.0;
+            frame.d = DataUtil.getLEShort(data, offset8Val + 12) / 4096.0;
+
+            animData.Frames.Add(frame);
+
             return animData;
+        }
+    }
+
+    public class AnimFrame
+    {
+        public double x, y, z;
+        public double a, b, c, d;
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("AnimFrame: (").Append(x).Append(", ");
+            sb.Append(y).Append(", ");
+            sb.Append(z).Append(") (");
+            sb.Append(a).Append(", ");
+            sb.Append(b).Append(", ");
+            sb.Append(c).Append(", ");
+            sb.Append(d).Append(")");
+            return sb.ToString();
         }
     }
 
     public class AnimData
     {
         public int NumBones;
+        public int Offset4Val;
+
+        public List<AnimFrame> Frames = new List<AnimFrame>();
 
         public override string ToString()
         {
             StringBuilder sb = new StringBuilder();
             sb.Append("Num Bones = ").Append(NumBones).Append("\n");
+            sb.Append("Offset 4 val = ").Append(Offset4Val).Append("\n");
+            foreach (var frame in Frames) {
+                sb.Append(frame.ToString());
+            }
             return sb.ToString();
         }
     }
