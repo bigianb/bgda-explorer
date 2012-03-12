f0dda1d4cebc92fa2cd33e025d07dd0e60687207
bigianb@gmail.com <bigianb@gmail.com@e1ae63d9-c44a-27ee-ff2d-4c7a2df71a3b>
2012-03-12 23:58:32 +0000

    animation decoding

--- trunk/WorldExplorer/WorldExplorer/DataLoaders/AnmDecoder.cs
+++ trunk/WorldExplorer/WorldExplorer/DataLoaders/AnmDecoder.cs
@@ -31,21 +31,66 @@ namespace WorldExplorer.DataLoaders
             AnimData animData = new AnimData();
             animData.NumBones = DataUtil.getLEInt(data, startOffset);
             animData.Offset4Val = DataUtil.getLEInt(data, startOffset + 4);
+            animData.Offset14Val = DataUtil.getLEInt(data, startOffset + 0x14);
+            animData.Offset18Val = DataUtil.getLEInt(data, startOffset + 0x18);
             int offset8Val = startOffset + DataUtil.getLEInt(data, startOffset + 8);
 
-            AnimFrame frame = new AnimFrame();
+            for (int bone = 0; bone < animData.NumBones; ++bone) {
+                AnimFrame frame = new AnimFrame();
+                int frameOff = offset8Val + bone * 0x0e;
 
-            frame.x = DataUtil.getLEShort(data, offset8Val) / 64.0;
-            frame.y = DataUtil.getLEShort(data, offset8Val+2) / 64.0;
-            frame.z = DataUtil.getLEShort(data, offset8Val+4) / 64.0;
+                frame.x = DataUtil.getLEShort(data, frameOff) / 64.0;
+                frame.y = DataUtil.getLEShort(data, frameOff + 2) / 64.0;
+                frame.z = DataUtil.getLEShort(data, frameOff + 4) / 64.0;
 
-            frame.a = DataUtil.getLEShort(data, offset8Val + 6) / 4096.0;
-            frame.b = DataUtil.getLEShort(data, offset8Val + 8) / 4096.0;
-            frame.c = DataUtil.getLEShort(data, offset8Val + 10) / 4096.0;
-            frame.d = DataUtil.getLEShort(data, offset8Val + 12) / 4096.0;
+                frame.a = DataUtil.getLEShort(data, frameOff + 6) / 4096.0;
+                frame.b = DataUtil.getLEShort(data, frameOff + 8) / 4096.0;
+                frame.c = DataUtil.getLEShort(data, frameOff + 0xA) / 4096.0;
+                frame.d = DataUtil.getLEShort(data, frameOff + 0xC) / 4096.0;
 
-            animData.Frames.Add(frame);
-
+                animData.Frames.Add(frame);
+            }
+            int otherOff = offset8Val + animData.NumBones * 0x0e;
+            var sb = new StringBuilder();
+            while (otherOff < endIndex) {
+                sb.Append("Count: ").Append(data[otherOff++]);
+                byte byte2 = data[otherOff++];
+                int bone = byte2 & 0x3f;
+                sb.Append(", Bone: ").Append(bone);
+                // bit 7 specifies whether to read 4 (set) or 3 elements following
+                // bit 6 specifies whether they are shorts or bytes (set).
+                if ((byte2 & 0x80) == 0x80) {
+                    int a, b, c, d;
+                    if ((byte2 & 0x40) == 0x40) {
+                        a = data[otherOff++];
+                        b = data[otherOff++];
+                        c = data[otherOff++];
+                        d = data[otherOff++];
+                    } else {
+                        a = DataUtil.getLEShort(data, otherOff);
+                        b = DataUtil.getLEShort(data, otherOff+2);
+                        c = DataUtil.getLEShort(data, otherOff+4);
+                        d = DataUtil.getLEShort(data, otherOff+6);
+                        otherOff += 8;
+                    }
+                    sb.Append(", (").Append(a).Append(", ").Append(b).Append(", ").Append(c).Append(", ").Append(d).Append(")");
+                } else {
+                    int x, y, z;
+                    if ((byte2 & 0x40) == 0x40) {
+                        x = data[otherOff++];
+                        y = data[otherOff++];
+                        z = data[otherOff++];
+                    } else {
+                        x = DataUtil.getLEShort(data, otherOff);
+                        y = DataUtil.getLEShort(data, otherOff + 2);
+                        z = DataUtil.getLEShort(data, otherOff + 4);
+                        otherOff += 6;
+                    }
+                    sb.Append(", (").Append(x).Append(", ").Append(y).Append(", ").Append(z).Append(")");
+                }
+                sb.Append("\n");
+            }
+            animData.Other = sb.ToString();
             return animData;
         }
     }
@@ -73,6 +118,10 @@ namespace WorldExplorer.DataLoaders
     {
         public int NumBones;
         public int Offset4Val;
+        public int Offset14Val;
+        public int Offset18Val;     // These are 4 bytes which are all ored together
+
+        public string Other;
 
         public List<AnimFrame> Frames = new List<AnimFrame>();
 
@@ -81,9 +130,12 @@ namespace WorldExplorer.DataLoaders
             StringBuilder sb = new StringBuilder();
             sb.Append("Num Bones = ").Append(NumBones).Append("\n");
             sb.Append("Offset 4 val = ").Append(Offset4Val).Append("\n");
+            sb.Append("Offset 0x14 val = ").Append(Offset14Val).Append("\n");
+            sb.Append("Offset 0x18 val = ").Append(Offset18Val).Append("\n");
             foreach (var frame in Frames) {
-                sb.Append(frame.ToString());
+                sb.Append(frame.ToString()).Append("\n");
             }
+            sb.Append(Other);
             return sb.ToString();
         }
     }
