3a501ff609d1dea2f0aa7de9f16bfed7d6879ec0
bigianb@gmail.com <bigianb@gmail.com@e1ae63d9-c44a-27ee-ff2d-4c7a2df71a3b>
2012-08-30 17:14:05 +0000

    Thinking about a level view.

---
+++ trunk/WorldExplorer/WorldExplorer/LevelView.xaml.cs
@@ -0,0 +1,54 @@
+﻿/*  Copyright (C) 2012 Ian Brown
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WorldExplorer.Tools3D;
+
+namespace WorldExplorer
+{
+    /// <summary>
+    /// Interaction logic for LevelView.xaml
+    /// </summary>
+    public partial class LevelView : UserControl
+    {
+        public LevelView()
+        {
+            InitializeComponent();
+
+            DataContextChanged += new DependencyPropertyChangedEventHandler(ModelView_DataContextChanged);
+        }
+
+        void ModelView_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            LevelViewModel model = (LevelViewModel)DataContext;
+            var trackball = new Trackball();
+            trackball.EventSource = trackballSource;
+            model.CameraTransform = trackball.Transform;
+        }
+    }
+}
