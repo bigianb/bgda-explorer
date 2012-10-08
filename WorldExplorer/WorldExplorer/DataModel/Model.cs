7b0e1775385ba2e627573db7e142c437ee0a0042
invisghost@gmail.com <invisghost@gmail.com@e1ae63d9-c44a-27ee-ff2d-4c7a2df71a3b>
2012-10-08 19:15:12 +0000

    GUI: * Added the ability to click on an animation file and apply it to the currently loaded model.
    Code:
    * Added a CountBones method to the Model class.

--- WorldExplorer/WorldExplorer/DataModel/Model.cs
+++ WorldExplorer/WorldExplorer/DataModel/Model.cs
@@ -9,5 +9,34 @@ namespace WorldExplorer.DataModel
     {
         // collection of meshes
         public List<Mesh> meshList;
+
+        /// <summary>
+        /// Counts the number of bones in the model based of bone id references.
+        /// </summary>
+        public int CountBones()
+        {
+            // Find the largest bone id
+            int largestID = 0;
+
+            foreach (var mesh in meshList)
+            {
+                foreach (var weight in mesh.vertexWeights)
+                {
+                    if (weight.bone1 != 255 && largestID < weight.bone1)
+                        largestID = weight.bone1;
+                    if (weight.bone2 != 255 && largestID < weight.bone2)
+                        largestID = weight.bone2;
+                    if (weight.bone3 != 255 && largestID < weight.bone3)
+                        largestID = weight.bone3;
+                    if (weight.bone4 != 255 && largestID < weight.bone4)
+                        largestID = weight.bone4;
+                }
+            }
+
+            if (largestID == 0)
+                return 0;
+
+            return largestID + 1;
+        }
     }
 }
