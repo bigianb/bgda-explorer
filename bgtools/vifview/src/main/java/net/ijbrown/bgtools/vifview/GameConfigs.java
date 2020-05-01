27634fb92e99d6208382f152dd9022a3faede460
Ian Brown <bigianb@gmail.com>
2020-05-01 17:27:54 +0100

    adding some config

---
+++ bgtools/vifview/src/main/java/net/ijbrown/bgtools/vifview/GameConfigs.java
@@ -0,0 +1,26 @@
+package net.ijbrown.bgtools.vifview;
+
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class GameConfigs {
+
+    public void read() throws IOException, URISyntaxException {
+        URL url = getClass().getClassLoader().getResource("net/ijbrown/bgtools/config/gamesConfig.json");
+        if (url == null){
+            throw new IOException("cannot build URL");
+        }
+        File configFile = new File(url.toURI());
+
+        Gson gson = new Gson();
+        gameConfigs = gson.fromJson(new FileReader(configFile), GameConfig[].class);
+    }
+
+    public GameConfig[] gameConfigs = null;
+
+}
