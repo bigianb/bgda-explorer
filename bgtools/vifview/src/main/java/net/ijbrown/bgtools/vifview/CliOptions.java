2f6550e22042e79301b4832eea3da980b2dcbf8a
Ian Brown <bigianb@gmail.com>
2020-05-05 18:39:44 +0100

    added configs

---
+++ bgtools/vifview/src/main/java/net/ijbrown/bgtools/vifview/CliOptions.java
@@ -0,0 +1,26 @@
+package net.ijbrown.bgtools.vifview;
+
+import com.google.devtools.common.options.Option;
+import com.google.devtools.common.options.OptionsBase;
+
+/**
+ * Command-line options.
+ */
+public class CliOptions extends OptionsBase {
+
+    @Option(
+            name = "help",
+            abbrev = 'h',
+            help = "Prints usage info.",
+            defaultValue = "false"
+    )
+    public boolean help;
+
+    @Option(
+            name = "dir",
+            abbrev = 'd',
+            help = "Name of directory where the game lives (where the SYSTEM.CNF file lives).",
+            defaultValue = ""
+    )
+    public String dir;
+}
