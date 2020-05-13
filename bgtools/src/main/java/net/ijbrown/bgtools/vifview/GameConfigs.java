bcf8608378b70600ac1a177d0ff333971c9289b6
Ian Brown <bigianb@gmail.com>
2020-05-13 10:44:51 +0100

    simplifying the structure

--- bgtools/src/main/java/net/ijbrown/bgtools/vifview/GameConfigs.java
+++ bgtools/src/main/java/net/ijbrown/bgtools/vifview/GameConfigs.java
@@ -7,11 +7,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLClassLoader;
 
 public class GameConfigs {
 
     public void read() throws IOException, URISyntaxException {
-        URL url = getClass().getClassLoader().getResource("net/ijbrown/bgtools/config/gamesConfig.json");
+
+        var loader = getClass().getClassLoader();
+
+        URL url = loader.getResource("net/ijbrown/bgtools/config/gamesConfig.json");
         if (url == null){
             throw new IOException("cannot build URL");
         }
