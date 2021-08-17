fb6411c29faac15bc074088a9765e70a75b36f7a
Ian Brown <bigianb@gmail.com>
2021-08-17 16:11:22 +0100

    HDR/DAT decoding

--- bgtools/src/main/java/net/ijbrown/bgtools/lmp/HdrDatDecode.java
+++ bgtools/src/main/java/net/ijbrown/bgtools/lmp/HdrDatDecode.java
@@ -15,8 +15,6 @@
 */
 package net.ijbrown.bgtools.lmp;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.*;
 
 /**
@@ -26,9 +24,12 @@ public class HdrDatDecode
 {
     public static void main(String[] args) throws IOException
     {
+        /*
         String inDir = "/emu/bgda/RTA/BG/DATA/";
-
         String outDir = "/emu/bgda/RTA/BG/DATA_extracted/";
+*/
+        String inDir = "/Users/ian/ps2_games/CON/BG/DATA/";
+        String outDir = "/Users/ian/ps2_games/CON/BG/DATA_extracted/";
 
         File outDirFile = new File(outDir);
         outDirFile.mkdirs();
@@ -40,6 +41,32 @@ public class HdrDatDecode
         String txt;
         txt = obj.disassemble();
         obj.writeFile("eqcache_hdr.txt", outDirFile, txt);
+
+        obj.extractFiles(inDirFile, outDirFile);
+    }
+
+    private void extractFiles(File dir, File outDirFile)throws IOException
+    {
+        File datFile = new File(dir, baseFilename+".DAT");
+        var entry = headerEntries[0];
+        for (int el=0; el<2; ++el){
+            var element = entry.headerElements[el];
+            var elData = readData(datFile, element.startPosBytes, element.lenBytes);
+
+
+            var elBaseName = baseFilename+"_"+entry.name+element.id+"_"+el;
+
+            File outFile = new File(outDirFile, elBaseName + ".dat");
+            BufferedOutputStream os = new BufferedOutputStream(new FileOutputStream(outFile));
+            os.write(elData, 0, element.lenBytes);
+            os.close();
+
+            // elements come in pairs, the first looks like a texture
+            if ((el & 1) == 0){
+                new TexDecode().extract(outDirFile, elData, 0, elBaseName, elData.length);
+            }
+
+        }
     }
 
     private void writeFile(String filename, File outDirFile, String txt) throws IOException
@@ -50,26 +77,59 @@ public class HdrDatDecode
         writer.close();
     }
 
-    private int fileLength;
-    private byte[] fileData;
+    static class HeaderEntry
+    {
+        String name;
+        HeaderElement[] headerElements;
+    }
+
+    private HeaderEntry[] headerEntries;
+
+    static class HeaderElement
+    {
+        int id;
+        int lenBytes;
+        int startPosBytes;
+    }
+
+    private byte[] headerFileData;
+    private String baseFilename;
+
+    private byte[] readData(File datFile, int start, int len) throws IOException
+    {
+        BufferedInputStream is = new BufferedInputStream(new FileInputStream(datFile));
+
+        var data = new byte[len];
+
+        var skipped = is.skip(start);
+        if (start != skipped){
+            throw new IOException("wrong number of bytes skipped");
+        }
+        is.readNBytes(data, 0, len);
+
+        is.close();
+        return data;
+    }
 
     public void read(String filename, File dir) throws IOException
     {
+        baseFilename = filename;
         File file = new File(dir, filename+".HDR");
-        read(file);
+        readHeader(file);
+        parseFileData();
     }
 
-    public void read(File file) throws IOException
+    public void readHeader(File file) throws IOException
     {
         BufferedInputStream is = new BufferedInputStream(new FileInputStream(file));
 
-        fileLength = (int) file.length();
-        fileData = new byte[fileLength];
+        int headerFileLength = (int) file.length();
+        headerFileData = new byte[headerFileLength];
 
         int offset = 0;
-        int remaining = fileLength;
+        int remaining = headerFileLength;
         while (remaining > 0) {
-            int read = is.read(fileData, offset, remaining);
+            int read = is.read(headerFileData, offset, remaining);
             if (read == -1) {
                 throw new IOException("Read less bytes then expected when reading file");
             }
@@ -78,34 +138,52 @@ public class HdrDatDecode
         }
     }
 
+    private void parseFileData()
+    {
+        int numEntries = DataUtil.getLEInt(headerFileData,  0);
+        headerEntries = new HeaderEntry[numEntries];
+
+        int offset=4;
+        for (int i=0; i<numEntries; ++i) {
+            int elOffset = DataUtil.getLEInt(headerFileData, offset);
+            int stringOffset = DataUtil.getLEInt(headerFileData, offset + 4);
+            int numEls = DataUtil.getLEInt(headerFileData, offset + 8);
+            String name = DataUtil.collectString(headerFileData, stringOffset);
+
+            var entry = new HeaderEntry();
+            entry.name = name;
+            entry.headerElements = new HeaderElement[numEls];
+            for (int el=0; el < numEls; ++el){
+                var element = new HeaderElement();
+                element.id = DataUtil.getLEShort(headerFileData, elOffset); elOffset += 2;
+                element.lenBytes = 2048 * DataUtil.getLEShort(headerFileData, elOffset); elOffset += 2;
+                element.startPosBytes = 2048 * DataUtil.getLEInt(headerFileData, elOffset); elOffset += 4;
+                entry.headerElements[el] = element;
+            }
+            headerEntries[i] = entry;
+            offset += 12;
+        }
+    }
+
+
     private String disassemble()
     {
         StringBuilder sb = new StringBuilder();
 
-        int numEntries = DataUtil.getLEInt(fileData,  0);
-
-        sb.append("Num entries: ").append(numEntries).append("\r\n");
+        sb.append("Num entries: ").append(headerEntries.length).append("\r\n");
 
         int offset=4;
-        for (int i=0; i<numEntries; ++i){
-           int elOffset = DataUtil.getLEInt(fileData, offset);
-           int stringOffset = DataUtil.getLEInt(fileData, offset+4);
-           int numEls = DataUtil.getLEInt(fileData, offset+8);
-
-           String name = DataUtil.collectString(fileData, stringOffset);
-
-           sb.append("\r\n");
-           sb.append("Name: ").append(name).append("\r\n");
-           sb.append("numEls: ").append(numEls).append("\r\n");
-           sb.append("Els: ").append("\r\n");
-           for (int el=0; el < numEls; ++el){
-               short s1 = DataUtil.getLEShort(fileData, elOffset); elOffset += 2;
-               short s2 = DataUtil.getLEShort(fileData, elOffset); elOffset += 2;
-               short s3 = DataUtil.getLEShort(fileData, elOffset); elOffset += 2;
-               short s4 = DataUtil.getLEShort(fileData, elOffset); elOffset += 2;
-               sb.append(s1).append(", ").append(s2).append(", ").append(s3).append(", ").append(s4).append("\r\n");
-           }
-           offset += 12;
+        for (int i=0; i<headerEntries.length; ++i){
+            var entry = headerEntries[i];
+            sb.append("\r\n");
+            sb.append("Name: ").append(entry.name).append("\r\n");
+            sb.append("numEls: ").append(entry.headerElements.length).append("\r\n");
+            sb.append("Els: ").append("\r\n");
+            for (int el=0; el < entry.headerElements.length; ++el){
+                var element = entry.headerElements[el];
+                sb.append("ID: ").append(element.id).append(", Len: ").append(element.lenBytes).append(", Start: ").append(element.startPosBytes).append("\r\n");
+            }
+            offset += 12;
         }
 
         return sb.toString();
